65afe558b23830998abecee5ab67c390e7741a6f
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-06 20:06:02 -0300

    Section 9, class 112

---
+++ src/components/Itens.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class Itens extends Component {
+    render() {
+        return (
+            <Text>Detalhes do Item</Text>
+        );
+    }
+}
