ffd20498cd059a8befaa0ff3dc6143dbc5386741
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-07 02:01:56 -0300

    Section 9, class 120

--- src/components/Itens.js
+++ src/components/Itens.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
 
 export default class Itens extends Component {
     render() {
         return (
-            <View>
-                <Image
-                    style={{ height: 100, width: 100 }}
-                    source={{ uri: this.props.item.foto }}
-                />
-                <Text>{this.props.item.titulo}</Text>
-                <Text>{this.props.item.valor}</Text>
-                <Text>{this.props.item.valor}</Text>
-                <Text>{this.props.item.local_anuncio}</Text>
-                <Text>{this.props.item.data_publicacao}</Text>
+            <View style={styles.item}>
+                <View style={styles.foto}>
+                    <Image
+                        style={{ height: 100, width: 100 }}
+                        source={{ uri: this.props.item.foto }}
+                    />
+                </View>
+
+                <View style={styles.detalhesItem}>
+                    <Text style={styles.txtTitulo}>{this.props.item.titulo}</Text>
+                    <Text style={styles.txtValor}>R$ {this.props.item.valor}</Text>
+                    <Text style={styles.txtDetalhes}>Local: {this.props.item.local_anuncio}</Text>
+                    <Text style={styles.txtDetalhes}>Dt publicação: {this.props.item.data_publicacao}</Text>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: '#FFF',
+        borderWidth: 0.5,
+        borderColor: '#999',
+        margin: 10,
+        padding: 10,
+        flexDirection: 'row'
+    },
+    foto: {
+        width: 102,
+        height: 102
+    },
+    detalhesItem: {
+        marginLeft: 10,
+        flex: 1
+    },
+    txtTitulo: {
+        fontSize: 18,
+        color: 'blue',
+        marginBottom: 2
+    },
+    txtValor: {
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+    txtDetalhes: {
+        fontSize: 16
+    }
+});
