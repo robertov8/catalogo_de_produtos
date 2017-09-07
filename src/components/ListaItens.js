54ff9be0a515af241cdbe1b2652b0157ef5a3c8a
Roberto Ribeiro <robertov82008@gmail.com>
2017-09-07 01:12:32 -0300

    Section 9, class 116

--- src/components/ListaItens.js
+++ src/components/ListaItens.js
@@ -1,11 +1,24 @@
 // @flow
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import axios from 'axios';
 
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+
+    componentWillMount() {
+        axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+            .then(response => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log('Error ao recuperar os dados');
+            });
+    }
+
     render() {
+        
         return (
             <View>
                 <Itens />
